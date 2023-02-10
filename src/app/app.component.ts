58c4aabb48e0a985df5eedd1d29568d95847925a
IT21218958 <it21218958@my.sliit.lk>
2023-02-10 20:33:10 +0530

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ExcelJSON';
+  convertedJson!: string;
+
+  fileUpload(event: any) {
+    console.log(event.target.files);
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      console.log(event);
+      let binaryData = event.target?.result;
+      let workbook = XLSX.read(binaryData, { type: 'binary' });
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+        console.log(data);
+        this.convertedJson = JSON.stringify(data, undefined, 4);
+      })
+      console.log(workbook);
+    }
+  }
+  
+}
